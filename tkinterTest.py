d5d4ec0b857cca945ee36ba636272e6abf2cdc7e
Samuel Martinez <smohuff@gmail.com>
2022-10-11 18:14:27 -0500

    tempConverter

--- tkinterTest.py
+++ tkinterTest.py
@@ -3,13 +3,14 @@ import tkinter.ttk as ttk
 
 window = tk.Tk()
 
-greeting = tk.Label(text="Hello, Tkinter", foreground="white", background="black", width=25, height=10)
-greeting.pack()
+greeting = tk.Label(text="Name:", foreground="white", background="black", width=25, height=10)
 
 entry = tk.Entry(fg="white", bg="black", width=25)
-entry.pack()
 
 button = tk.Button(text="Click me", width=25, height=5, bg="white", fg="black")
+
+greeting.pack()
+entry.pack()
 button.pack()
 
 window.mainloop()
